d040d5650a906f18259a90179475f84f0ee2c29e
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-07-09 13:51:22 +0900

    Update main.py

--- line_bot/main.py
+++ line_bot/main.py
@@ -16,7 +16,7 @@ from linebot.models import (
 app = Flask(__name__)
 res=Response()
 
-#Herokuの変数からトークンなどを取得
+# Herokuの変数からトークンなどを取得
 channel_secret = os.environ['LINE_CHANNEL_SECRET']
 channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 if channel_secret is None:
@@ -29,14 +29,14 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-#LINEからのWebhook
-@app.route("/callback", methods=['POST'])
+# LINEからのWebhook
+@app.route("/callback", methods = ['POST'])
 def callback():
     # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
     
     # リクエストボディを取得
-    body = request.get_data(as_text=True)
+    body = request.get_data(as_text = True)
     app.logger.info("Request body: " + body)
 
     # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
@@ -46,16 +46,18 @@ def callback():
         abort(400)
     return 'OK'
 
-#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合
-#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
-@handler.add(MessageEvent, message=TextMessage)
+"""
+LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合
+reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+"""
+@handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
     
-    #入力された内容(event.message.text)に応じて返信する
+    # 入力された内容(event.message.text)に応じて返信する
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=os.environ[res.getResponse(event.message.text)])
+    TextSendMessage(text = os.environ[res.getResponse(event.message.text)])
     )
 
 if __name__ == "__main__":
