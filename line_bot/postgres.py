d1338809496a4234f5a58844a05de36b3c20aaaf
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-08-06 16:33:36 +0900

    Update postgres.py

--- line_bot/postgres.py
+++ line_bot/postgres.py
@@ -14,6 +14,14 @@ class Postgres:
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query % user_id)
+    
+    def get_users(self, channel_id):
+        query = "SELECT (user_id FROM user_info ;"
+        with self.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+                return cur.fetchall()
+            
     """            
     def create_table(self):
         query="CREATE TABLE user_info (id serial, user_id varchar(50) NOT NULL);"
