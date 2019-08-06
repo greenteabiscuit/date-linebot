a49fb5cfedd912c3c97a3f46d4ff23912acd7ba7
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-08-06 16:31:00 +0900

    Update postgres.py

--- line_bot/postgres.py
+++ line_bot/postgres.py
@@ -9,15 +9,15 @@ class Postgres:
         return psycopg2.connect(os.environ["DATABASE_URL"])
         
     # 新規登録            
-    def insert(self, user_id = None):
+    def register_user(self, user_id = None):
         query = "INSERT INTO user_info (user_id) VALUES('%s');"
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query % user_id)
-                
+    """            
     def create_table(self):
         query="CREATE TABLE user_info (id serial, user_id varchar(50) NOT NULL);"
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query)
- 
+    """
