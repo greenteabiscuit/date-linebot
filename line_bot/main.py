d8e652d8da6098a2b11e2c0dfe6579255139f601
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-08-06 16:27:07 +0900

    Update main.py

--- line_bot/main.py
+++ line_bot/main.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent,
 )
 from postgres import Postgres
 
