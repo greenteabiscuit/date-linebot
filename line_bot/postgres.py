53847fbf1b6b92831ef1a50f0a146177523366ea
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-08-13 15:47:44 +0900

    Update postgres.py

--- line_bot/postgres.py
+++ line_bot/postgres.py
@@ -8,8 +8,10 @@ class Postgres:
     def get_connection(self):
         return psycopg2.connect(os.environ["DATABASE_URL"])
     
+    """
     def get_connect(self):
         return psycopg2.connect(os.environ["DB_DANCE"])
+    """
         
     # 新規登録            
     def register_user(self, user_id = None):
@@ -35,9 +37,10 @@ class Postgres:
                 else:
                     return False
                 
-               
+    """           
     def create_table(self):
         query = "CREATE TABLE cotanct (id serial, user_id varchar(50), name varchar(50), kana varchar(50), mail varchar(100), title varchar(50), text varchar(500), date date, time time);"
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query)
+    """
